76feff929a1da85701a5c1c79fbdae71cfe9a657
marek311 <mareklacko311@icloud.com>
2024-10-20 13:23:45 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ function App() {
       <Router>
           <div className="flex items-center justify-center min-h-screen bg-gray-900">
               <Routes>
-                  <Route path="/" element={
-                      <div className="text-center p-6 bg-purple-800 rounded-lg shadow-lg">
-                          <h1 className="text-white text-4xl font-bold mb-6">Heuristiky a metaheuristiky</h1>
-                          <Selector />
-                      </div>
-                  } />
-                  <Route path="/visualization" element={<InputReader />} />
+                  <Route path="/" element={<Selector/>}/>
+                  <Route path="/visualization" element={<InputReader/>}/>
               </Routes>
           </div>
       </Router>
--- src/Selector.js
+++ src/Selector.js
@@ -3,21 +3,22 @@ import { useNavigate } from 'react-router-dom';
 
 function Selector() {
     const heuristics = [
-        { id: 'Insert', name: 'Insert heuristika' },
-        { id: 'ExchangeFirst', name: 'Exchange heuristika - 1. vhodný' },
-        { id: 'ExchangeBest', name: 'Exchange heuristika - najlepší vhodný' },
-        { id: 'Annealing', name: 'Simulated annealing' },
-        { id: 'Tabu', name: 'Tabu search' },
-        { id: 'Genetic', name: 'Genetický algoritmus' },
+        {id: 'Insert', name: 'Insert heuristika'},
+        {id: 'ExchangeFirst', name: 'Exchange heuristika - 1. vhodný'},
+        {id: 'ExchangeBest', name: 'Exchange heuristika - najlepší vhodný'},
+        {id: 'Annealing', name: 'Simulated annealing'},
+        {id: 'Tabu', name: 'Tabu search'},
+        {id: 'Genetic', name: 'Genetický algoritmus'},
     ];
 
     const problems = [
-        { id: 'Knapsack', name: 'Úloha o batohu' },
-        { id: 'TSP', name: 'Problém obchodného cestujúceho' },
+        {id: 'Knapsack', name: 'Úloha o batohu'},
+        {id: 'TSP', name: 'Problém obchodného cestujúceho'},
     ];
 
     const [selectedHeuristic, setSelectedHeuristic] = useState(heuristics[0].id);
     const [selectedProblem, setSelectedProblem] = useState(problems[0].id);
+
     const navigate = useNavigate();
 
     const handleHeuristicChange = (event) => {
@@ -32,44 +33,54 @@ function Selector() {
         navigate(`/visualization?heuristic=${selectedHeuristic}&problem=${selectedProblem}`);
     };
 
-    return (
-        <div className="flex flex-col items-center mt-4">
-            <label className="mb-4 text-white">
-                Vyberte heuristiku:
-                <select
-                    value={selectedHeuristic}
-                    onChange={handleHeuristicChange}
-                    className="ml-2 p-2 border border-gray-300 rounded bg-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
-                >
-                    <option value="">-- Vyberte --</option>
-                    {heuristics.map((heuristic) => (
-                        <option key={heuristic.id} value={heuristic.id}>
-                            {heuristic.name}
-                        </option>
-                    ))}
-                </select>
-            </label>
+    const selectProblemAndHeuristic = () => {
+        return (
+        <div className="text-center p-6 bg-purple-800 rounded-lg shadow-lg flex flex-col items-center mt-4">
+            <h1 className="text-white text-4xl font-bold mb-6">Heuristiky a metaheuristiky</h1>
+                <label className="mb-4 text-white">
+                    Vyberte heuristiku:
+                    <select
+                        value={selectedHeuristic}
+                        onChange={handleHeuristicChange}
+                        className="ml-2 p-2 border border-gray-300 rounded bg-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    >
+                        <option value="">-- Vyberte --</option>
+                        {heuristics.map((heuristic) => (
+                            <option key={heuristic.id} value={heuristic.id}>
+                                {heuristic.name}
+                            </option>
+                        ))}
+                    </select>
+                </label>
 
-            <label className="mb-4 text-white">
-                Vyberte problém:
-                <select
-                    value={selectedProblem}
-                    onChange={handleProblemChange}
-                    className="ml-2 p-2 border border-gray-300 rounded bg-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
-                >
-                    <option value="">-- Vyberte --</option>
-                    {problems.map((problem) => (
-                        <option key={problem.id} value={problem.id}>
-                            {problem.name}
-                        </option>
-                    ))}
-                </select>
-            </label>
-            <button className="mt-4 px-4 py-2 bg-teal-600 text-white rounded hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                <label className="mb-4 text-white">
+                    Vyberte problém:
+                    <select
+                        value={selectedProblem}
+                        onChange={handleProblemChange}
+                        className="ml-2 p-2 border border-gray-300 rounded bg-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    >
+                        <option value="">-- Vyberte --</option>
+                        {problems.map((problem) => (
+                            <option key={problem.id} value={problem.id}>
+                                {problem.name}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <button
+                    className="mt-4 px-4 py-2 bg-teal-600 text-white rounded hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
                     onClick={handleSubmit}>
-                Ďaľej
-            </button>
-        </div>
+                    Ďaľej
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            {selectProblemAndHeuristic()}
+        </>
     );
 }
 
