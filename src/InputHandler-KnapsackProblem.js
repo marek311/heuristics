df548dcea6f6d76cef7070c68bea85a51dbe688f
marek311 <mareklacko311@icloud.com>
2024-11-13 11:50:09 +0100

    button centered fix

--- src/InputHandler-KnapsackProblem.js
+++ src/InputHandler-KnapsackProblem.js
@@ -42,7 +42,7 @@ function InputHandlerKnapsackProblem() {
                 onChange={(e) => setCapacity(e.target.value)}
             />
 
-            <div className="flex space-x-4 mt-4">
+            <div className="items-center justify-center flex space-x-4 mt-4">
                 <button onClick={handleGoBack} className="px-4 py-2 bg-red-800 rounded hover:bg-red-950">
                     Späť
                 </button>
