ef31052e037f9686b56bb0bc4c0a74f616e4d798
marek311 <mareklacko311@icloud.com>
2024-11-14 13:56:41 +0100

    insert heuristic for knapsack problem  display simulation process with step functionality styling update

--- src/SimulatorKnapsackInsert.js
+++ src/SimulatorKnapsackInsert.js
@@ -18,13 +18,12 @@ function SimulationKnapsackInsert() {
     const [currentWeight, setCurrentWeight] = useState(0);
     const [currentPrice, setCurrentPrice] = useState(0);
     const [selectedItems, setSelectedItems] = useState([]);
-
     const [itemStatus, setItemStatus] = useState(new Array(items.length).fill(null));
 
     const handleStep = () => {
         if (currentIndex < items.length) {
             const item = items[currentIndex];
-            let newItemStatus = [...itemStatus];
+            const newItemStatus = [...itemStatus];
             if (currentWeight + item.weight <= capacity) {
                 setSelectedItems([...selectedItems, item]);
                 setCurrentWeight(currentWeight + item.weight);
@@ -39,38 +38,45 @@ function SimulationKnapsackInsert() {
     };
 
     return (
-        <div
-            className="mb-4 flex flex-col items-center justify-center w-fit h-fit text-white p-6 bg-purple-600 rounded-lg mx-auto my-10">
-            <h2> Knapsack Insert Simulation </h2>
-            <p><strong>Capacity:</strong> {capacity}</p>
-
-            <h3>All Items:</h3>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>
-                        Weight: {item.weight}, Price: {item.price}, Efficiency: {item.efficiency.toFixed(2)}
-                        {itemStatus[index] === true && " ✓"}
-                        {itemStatus[index] === false && " ✗"}
-                    </li>
-                ))}
-            </ul>
-
-            <h3>Current Solution:</h3>
-            <p><strong>Current Weight:</strong> {currentWeight}</p>
-            <p><strong>Current Price:</strong> {currentPrice}</p>
-            <p><strong>Selected Items:</strong></p>
-            <ul>
-                {selectedItems.map((item, index) => (
-                    <li key={index}>
-                        Weight: {item.weight}, Price: {item.price}, Efficiency: {item.efficiency.toFixed(2)}
-                    </li>
-                ))}
-            </ul>
-
-            <button onClick={handleStep} className="px-4 py-2 bg-teal-700 rounded hover:bg-purple-700">
-                Step
-            </button>
-            {currentIndex >= items.length && <p>Algoritmus dokončený</p>}
+        <div className="flex flex-col lg:flex-row w-full h-full text-white p-6 bg-gray-900">
+            <div className="flex-1 p-4 bg-purple-700 rounded-lg mr-2">
+                <div className="flex justify-between mb-4">
+                    <h2>Predmety k dispozícií</h2>
+                    <p>Kapacita batohu: {capacity}</p>
+                </div>
+                <ul className="mt-4 space-y-2">
+                    {items.map((item, index) => (
+                        <li key={index} className="flex justify-between items-center p-2 bg-purple-500 rounded">
+                            <span>
+                                Váha: {item.weight}, Cena: {item.price}, Výhodnosť: {item.efficiency.toFixed(2)}
+                            </span>
+                            <span>
+                                {itemStatus[index] === true ? "✓" : itemStatus[index] === false ? "✗" : ""}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="flex-1 p-4 bg-purple-800 rounded-lg ml-2">
+                <div className="flex justify-between mb-4">
+                    <p>Aktuálna váha: {currentWeight}</p>
+                    <p>Aktuálna cena: {currentPrice}</p>
+                </div>
+                <div className="flex justify-between mb-4">
+                    <p><strong>Vybrané predmety:</strong></p>
+                    <button onClick={handleStep} className="mt-4 px-4 py-2 bg-teal-700 rounded hover:bg-teal-600">
+                        Krok
+                    </button>
+                </div>
+                <ul className="mt-2 space-y-2">
+                    {selectedItems.map((item, index) => (
+                        <li key={index} className="p-2 bg-purple-600 rounded">
+                        Váha: {item.weight}, Cena: {item.price}, Výhodnosť: {item.efficiency.toFixed(2)}
+                        </li>
+                    ))}
+                </ul>
+                {currentIndex >= items.length && <p className="mt-2">Algoritmus dokončený</p>}
+            </div>
         </div>
     );
 }
