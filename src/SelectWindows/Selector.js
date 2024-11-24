da012f993a1aec65fb5cafe1860db01c32246367
marek311 <mareklacko311@icloud.com>
2024-11-24 13:29:27 +0100

    new color palette - start

--- src/SelectWindows/Selector.js
+++ src/SelectWindows/Selector.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Selector() {
@@ -26,14 +26,14 @@ function Selector() {
 
     const selectMode = () => {
         return (
-        <div className="text-center p-6 bg-purple-800 rounded-lg shadow-lg flex flex-col items-center mt-4">
-            <h1 className="text-white text-4xl font-bold mb-6">Heuristiky a metaheuristiky</h1>
-                <label className="mb-4 text-white">
-                    Vyberte mód:
+            <div className="text-center p-6 bg-white rounded-lg shadow-lg flex flex-col items-center mt-4">
+                <h1 className="text-4xl font-bold mb-6">Heuristiky a metaheuristiky</h1>
+                <label className="mb-4 text-gray-800">
+                    Mód:
                     <select
                         value={selectedMode}
                         onChange={handleModeChange}
-                        className="ml-2 p-2 border border-gray-300 rounded bg-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                        className="ml-2 p-2 border border-gray-300 rounded bg-beige-200 text-gray-800 focus:outline-none focus:ring-2"
                     >
                         {modes.map((mode) => (
                             <option key={mode.id} value={mode.id}>
@@ -43,7 +43,7 @@ function Selector() {
                     </select>
                 </label>
                 <button
-                    className="mt-4 px-4 py-2 bg-teal-700 text-white rounded hover:bg-teal-600"
+                    className="px-4 py-2 bg-teal-500 rounded hover:bg-teal-400"
                     onClick={handleSubmit}>
                     Ďaľej
                 </button>
