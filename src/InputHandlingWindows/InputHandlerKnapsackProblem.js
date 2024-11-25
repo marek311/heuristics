e6ece77c2ce24d8de9ad87736bd8d293f5e32527
marek311 <mareklacko311@icloud.com>
2024-11-25 09:18:07 +0100

    refactoring

--- src/InputHandlingWindows/InputHandlerKnapsackProblem.js
+++ src/InputHandlingWindows/InputHandlerKnapsackProblem.js
@@ -60,36 +60,40 @@ function InputHandlerKnapsackProblem( {mode} ) {
         navigate(simulationPath, { state: { mode, data: inputData } });
     };
 
-    return (
-        <div>
-            <label className="block text-gray-800 mb-2">
-                CSV súbor: riadok obsahuje cenu a hmotnosť vo formáte: cena;hmotnosť
-            </label>
-            <input
-                type="file"
-                accept=".csv"
-                className="p-2 mb-4 text-black border rounded w-full"
-                onChange={handleFileUpload}
-            />
-
-            <label className="block text-gray-800 mb-2">Zadajte kapacitu batohu:</label>
-            <input
-                type="number"
-                className="p-2 mb-4 text-black border rounded w-full"
-                placeholder="Kapacita batohu"
-                value={capacity}
-                onChange={(e) => setCapacity(e.target.value)}
-            />
-
-            <div className="items-center justify-center flex space-x-4 mt-4">
-                <button onClick={handleGoBack} className="px-4 py-2 bg-red-500 rounded hover:bg-red-600">
-                    Späť
-                </button>
-                <button onClick={handleRunClick} className="px-4 py-2 bg-teal-500 rounded hover:bg-teal-400">
-                    Ďaľej
-                </button>
+    const handleInputs = () => {
+        return (
+            <div>
+                <label className="block text-gray-800 mb-2">
+                    CSV súbor: riadok obsahuje cenu a hmotnosť vo formáte: cena;hmotnosť
+                </label>
+                <input
+                    type="file"
+                    accept=".csv"
+                    className="p-2 mb-4 text-black border rounded w-full"
+                    onChange={handleFileUpload}
+                />
+                <label className="block text-gray-800 mb-2">Zadajte kapacitu batohu:</label>
+                <input
+                    type="number"
+                    className="p-2 mb-4 text-black border rounded w-full"
+                    placeholder="Kapacita batohu"
+                    value={capacity}
+                    onChange={(e) => setCapacity(e.target.value)}
+                />
+                <div className="items-center justify-center flex space-x-4 mt-4">
+                    <button onClick={handleGoBack} className="px-4 py-2 bg-red-500 rounded hover:bg-red-600">
+                        Späť
+                    </button>
+                    <button onClick={handleRunClick} className="px-4 py-2 bg-teal-500 rounded hover:bg-teal-400">
+                        Ďaľej
+                    </button>
+                </div>
             </div>
-        </div>
+        );
+    }
+
+    return (
+        <> {handleInputs()} </>
     );
 }
 
