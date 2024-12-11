939e41a0f2f8bfceff19f6dced34d79de275c14d
marek311 <mareklacko311@icloud.com>
2024-12-11 19:46:32 +0100

    button text styling fix

--- src/Simulators/SimulationKnapsackExchange/SimKnapsackExchange.js
+++ src/Simulators/SimulationKnapsackExchange/SimKnapsackExchange.js
@@ -139,7 +139,7 @@ function SimulationKnapsackExchange() {
     return (
         <div className="flex flex-col w-full h-full text-gray-800 p-6">
             <div className="flex justify-between items-center p-4 bg-white rounded-lg mb-4">
-                <button onClick={handleGoBack} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
+                <button onClick={handleGoBack} className="px-4 py-2 bg-red-500 rounded hover:bg-red-600">
                     Späť
                 </button>
                 <h2 className="text-lg font-semibold">
