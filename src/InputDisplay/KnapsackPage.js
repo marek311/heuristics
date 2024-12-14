b8e1b12b92671fe369bb84e6ea173cbd64c49141
marek311 <mareklacko311@icloud.com>
2024-12-14 13:25:30 +0100

    code cleanup

--- src/InputDisplay/KnapsackPage.js
+++ src/InputDisplay/KnapsackPage.js
@@ -22,28 +22,25 @@ function KnapsackPage() {
     };
 
     return (
-        <div>
-            <div
-                className={`mb-4 flex flex-col items-center justify-center w-fit h-fit ${Colors.textPrimary} p-6 ${Colors.cardBackground} rounded-lg mx-auto my-10`}>
-                <KnapsackData
-                    items={items}
-                    capacity={capacity}
-                    simpleMode={true}
-                    showStatus={false}
-                    highlightCurrent={false}
-                />
-                <div className="flex justify-between mb-4 space-x-4 mt-4">
-                    <button
-                        className={`px-2 py-2 rounded ${Colors.buttonSecondary} ${Colors.buttonSecondaryHover}`}
-                        onClick={() => navigate(-1)} >
-                        Späť
-                    </button>
-                    <button
-                        className={`px-2 py-2 rounded ${Colors.buttonPrimary} ${Colors.buttonPrimaryHover}`}
-                        onClick={handleRunClick} >
-                        Spusti
-                    </button>
-                </div>
+        <div className={`mb-4 flex flex-col items-center justify-center w-fit h-fit ${Colors.textPrimary} p-6 ${Colors.cardBackground} rounded-lg mx-auto my-10`}>
+            <KnapsackData
+                items={items}
+                capacity={capacity}
+                simpleMode={true}
+                showStatus={false}
+                highlightCurrent={false}
+            />
+            <div className="flex justify-between mb-4 space-x-4 mt-4">
+                <button
+                    className={`px-2 py-2 rounded ${Colors.buttonSecondary} ${Colors.buttonSecondaryHover}`}
+                    onClick={() => navigate(-1)}>
+                    Späť
+                </button>
+                <button
+                    className={`px-2 py-2 rounded ${Colors.buttonPrimary} ${Colors.buttonPrimaryHover}`}
+                    onClick={handleRunClick}>
+                    Spusti
+                </button>
             </div>
         </div>
     );
