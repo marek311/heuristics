862367de4fe637402030e07ea0ee5e0eb2ca717b
marek311 <mareklacko311@icloud.com>
2024-12-14 12:47:28 +0100

    default data update

--- src/InputHandling/DefaultDataKnapsack.js
+++ src/InputHandling/DefaultDataKnapsack.js
@@ -1,7 +1,7 @@
 function DefaultDataKnapsack() {
     return {
         weights: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        prices: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+        prices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         capacity: 5
     };
 }
