325110d8e3931ef26edac4fb5bb2d92eddd1f84a
marek311 <mareklacko311@icloud.com>
2024-12-14 14:42:25 +0100

    buttons placement update

--- src/InputDisplay/KnapsackPage.js
+++ src/InputDisplay/KnapsackPage.js
@@ -30,7 +30,7 @@ function KnapsackPage() {
                 showStatus={false}
                 highlightCurrent={false}
             />
-            <div className="flex justify-between w-full px-12 mb-4 mt-4">
+            <div className="flex justify-between w-full px-12">
                 <button
                     className={`px-2 py-2 rounded ${Colors.buttonSecondary} ${Colors.buttonSecondaryHover}`}
                     onClick={() => navigate(-1)}>
