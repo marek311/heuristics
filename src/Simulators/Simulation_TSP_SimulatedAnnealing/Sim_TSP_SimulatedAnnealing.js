c69be1aa3864cfa452204ace21fa6760b5cf4261
marek311 <mareklacko311@icloud.com>
2025-01-25 12:33:42 +0100

    simulation component initial solution

--- src/Simulators/Simulation_TSP_SimulatedAnnealing/Sim_TSP_SimulatedAnnealing.js
+++ src/Simulators/Simulation_TSP_SimulatedAnnealing/Sim_TSP_SimulatedAnnealing.js
@@ -8,89 +8,41 @@ function SimulationTSPAnnealing() {
     const location = useLocation();
 
     const { data } = location.state || {};
-    const [tour, setTour] = useState([]);
-    const [temperature, setTemperature] = useState(100);
-    const [coolingRate] = useState(0.99);
-    const [iteration, setIteration] = useState(0);
+
+    const [currentTour, setCurrentTour] = useState([]);
     const [currentCost, setCurrentCost] = useState(0);
 
     useEffect(() => {
-        if (data && data.edges && data.edges.length > 0) {
-            const greedyTour = [];
-            const visitedCities = new Set();
-            let currentCity = data.edges[0].city1;
+        if (data && data.edges) {
+            const cities = Array.from(new Set(data.edges.flatMap((edge) => [edge.city1, edge.city2])));
+            let randomTour = [...cities].sort(() => Math.random() - 0.5);
 
-            greedyTour.push(currentCity);
-            visitedCities.add(currentCity);
+            randomTour.push(randomTour[0]);
+            setCurrentTour(randomTour);
 
-            const getDistance = (city1, city2) => {
-                const edge = data.edges.find(
-                    (edge) =>
-                        (edge.city1 === city1 && edge.city2 === city2) ||
-                        (edge.city1 === city2 && edge.city2 === city1)
+            let totalCost = 0;
+            for (let i = 0; i < randomTour.length - 1; i++) {
+                const edge = data.edges.find((e) =>
+                    (e.city1 === randomTour[i] && e.city2 === randomTour[i + 1]) ||
+                    (e.city1 === randomTour[i + 1] && e.city2 === randomTour[i])
                 );
-                return edge ? edge.distance : Infinity;
-            };
-
-            while (visitedCities.size < data.cityCount) {
-                let nearestCity = null;
-                let shortestDistance = Infinity;
-
-                for (const edge of data.edges) {
-                    const nextCity = edge.city1 === currentCity && !visitedCities.has(edge.city2) ? edge.city2 :
-                        (edge.city2 === currentCity && !visitedCities.has(edge.city1) ? edge.city1 : null);
-                    if (nextCity) {
-                        const distance = getDistance(currentCity, nextCity);
-                        if (distance < shortestDistance) {
-                            nearestCity = nextCity;
-                            shortestDistance = distance;
-                        }
-                    }
-                }
-
-                if (nearestCity) {
-                    greedyTour.push(nearestCity);
-                    visitedCities.add(nearestCity);
-                    currentCity = nearestCity;
-                }
+                totalCost += edge.distance;
             }
 
-            greedyTour.push(greedyTour[0]);
-
-            setTour(greedyTour);
-
-            const calculateCost = (tour) => {
-                let cost = 0;
-                for (let i = 0; i < tour.length - 1; i++) {
-                    const currentCity = tour[i];
-                    const nextCity = tour[i + 1];
-                    const edge = data.edges.find(
-                        (edge) =>
-                            (edge.city1 === currentCity && edge.city2 === nextCity) ||
-                            (edge.city1 === nextCity && edge.city2 === currentCity)
-                    );
-                    if (edge) {
-                        cost += edge.distance;
-                    }
-                }
-                return cost;
-            };
-
-            const initialCost = calculateCost(greedyTour);
-            setCurrentCost(initialCost);
+            setCurrentCost(totalCost);
         }
     }, [data]);
 
     const handleIteration = () => {
-        // To be implemented
+        // TODO
     };
 
     const handleRun = () => {
-        // To be implemented
+        // TODO
     };
 
     const handleReset = () => {
-        // To be implemented
+        // TODO
     };
 
     return (
@@ -100,23 +52,21 @@ function SimulationTSPAnnealing() {
                 title="TSP Simulation Using Simulated Annealing"
             />
             <div className="flex flex-col lg:flex-row w-full h-full">
-                <TSPDataGraph data={data}/>
-
+                <TSPDataGraph data={data} tour={currentTour}/>
                 <div className="p-4 bg-white rounded-lg shadow-md w-full lg:w-1/3">
                     <h2 className="text-lg font-semibold mb-4">Simulation Details</h2>
                     <ul className="space-y-2">
-                        <li>Initial Cost: {currentCost.toFixed(2)}</li>
-                        <li>Iteration: {iteration}</li>
-                        <li>Current Temperature: {temperature.toFixed(2)}</li>
+                        <li>Initial Cost: {currentCost}</li>
+                        <li>Iteration: 0</li>
+                        <li>Current Temperature:</li>
                         <li>
                             <strong>Current Tour:</strong>
                             <ul>
-                                {tour.map((city, index) => (
+                                {currentTour.map((city, index) => (
                                     <li key={index}>{city}</li>
                                 ))}
                             </ul>
                         </li>
-
                     </ul>
                 </div>
             </div>
