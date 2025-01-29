d1a68f15019a1d65436e04a53b92dbb16a955fe1
marek311 <mareklacko311@icloud.com>
2025-01-29 14:49:27 +0100

    solution display improved

--- src/Simulators/Simulation_TSP_SimulatedAnnealing/Sol_TSP_SimulatedAnnealing.js
+++ src/Simulators/Simulation_TSP_SimulatedAnnealing/Sol_TSP_SimulatedAnnealing.js
@@ -17,18 +17,11 @@ function SolTSPSimAnnealing({
             <div className="text-center text-lg">
                 Iteration: {iteration}
             </div>
-            <div className="bg-red-100 p-2 rounded-lg mb-2">
-                <h2 className="text-lg font-semibold text-red-600">Current Solution</h2>
-                <ul className="space-y-2 text-gray-800">
-                    <li><strong>Current Cost:</strong> {currentCost}</li>
-                    <li>
-                        <p>{currentTour.join(', ')}</p>
-                    </li>
-                </ul>
-            </div>
             <div className="bg-blue-100 p-2 rounded-lg mb-2">
                 <ul className="space-y-2 text-gray-800">
-                    <h2 className="text-lg font-semibold text-blue-600">Proposed Solution</h2>
+                    <div className="bg-blue-200 p-2 rounded-lg mb-2">
+                        <h2 className="text-lg font-semibold text-blue-600">Proposed Solution</h2>
+                    </div>
                     <ul className="space-y-2 text-gray-800">
                         <li><strong>Proposed Cost:</strong> {proposedCost}</li>
                         <li>
@@ -40,8 +33,21 @@ function SolTSPSimAnnealing({
                     <li><strong>Random Value:</strong> {randomValue.toFixed(4)}</li>
                 </ul>
             </div>
+            <div className="bg-red-100 p-2 rounded-lg mb-2">
+                <div className="bg-red-200 p-2 rounded-lg mb-2">
+                    <h2 className="text-lg font-semibold text-red-600">Current Solution</h2>
+                </div>
+                <ul className="space-y-2 text-gray-800">
+                    <li><strong>Current Cost:</strong> {currentCost}</li>
+                    <li>
+                        <p>{currentTour.join(', ')}</p>
+                    </li>
+                </ul>
+            </div>
             <div className="bg-green-100 p-2 rounded-lg mb-2">
-                <h2 className="text-lg font-semibold text-green-600">Best Found Solution</h2>
+                <div className="bg-green-200 p-2 rounded-lg mb-2">
+                    <h2 className="text-lg font-semibold text-green-600">Best Found Solution</h2>
+                </div>
                 <ul className="space-y-2 text-gray-800">
                     <li><strong>Best Cost:</strong> {bestCost}</li>
                     <li>
