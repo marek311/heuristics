7b283ec2bb286950e52973b097517bc1a889a17c
marek311 <mareklacko311@icloud.com>
2025-02-09 09:22:04 +0100

    tabu move expiration

--- src/Simulators/Simulation_TSP_TabuSearch/TabuTable.js
+++ src/Simulators/Simulation_TSP_TabuSearch/TabuTable.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function TabuTable({ tabuList }) {
     return (
         <div className="bg-white shadow-md rounded-lg p-4 w-full lg:w-1/3">
@@ -8,7 +6,8 @@ function TabuTable({ tabuList }) {
                 <thead>
                 <tr className="bg-gray-200">
                     <th className="border border-gray-300 p-2">Iteration</th>
-                    <th className="border border-gray-300 p-2">Swap (Index 1 - Index 2)</th>
+                    <th className="border border-gray-300 p-2">Swap</th>
+                    <th className="border border-gray-300 p-2">Expires</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -17,11 +16,12 @@ function TabuTable({ tabuList }) {
                         <tr key={index} className="border border-gray-300">
                             <td className="border border-gray-300 p-2 text-center">{entry.iteration}</td>
                             <td className="border border-gray-300 p-2 text-center">{`${entry.swap[0]} - ${entry.swap[1]}`}</td>
+                            <td className="border border-gray-300 p-2 text-center">{entry.expiryIteration}</td>
                         </tr>
                     ))
                 ) : (
                     <tr>
-                        <td colSpan="2" className="p-2 text-center text-gray-500">No tabu moves recorded</td>
+                        <td colSpan="3" className="p-2 text-center text-gray-500">No tabu moves recorded</td>
                     </tr>
                 )}
                 </tbody>
