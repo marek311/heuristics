3f2179dffe15c4eab005710526d53a5221e52fb2
marek311 <mareklacko311@icloud.com>
2025-02-19 15:33:35 +0100

    standalone component for population display

--- src/Simulators/TSP/SimulationGeneticAlgorithm/MainComponentGeneticAlgorithm.js
+++ src/Simulators/TSP/SimulationGeneticAlgorithm/MainComponentGeneticAlgorithm.js
@@ -6,6 +6,7 @@ import {
     rouletteWheelSelection
 } from './AlgorithmsGeneticAlgorithm';
 import SelectionComponent from "./SelectionComponent";
+import PopulationComponent from "./PopulationComponent";
 import RouletteWheel from "./RouletteWheel";
 
 function MainComponentGeneticAlgorithm() {
@@ -42,24 +43,10 @@ function MainComponentGeneticAlgorithm() {
                 title="TSP Simulation Using Genetic Algorithm"
             />
             <div className="flex flex-col lg:flex-row w-full h-full lg:space-x-2">
-                <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-3xl">
-                    <h2 className="text-xl font-semibold text-center">Initial Population</h2>
-                    {population.map((tour, index) => (
-                        <div
-                            key={index}
-                            className="flex items-center justify-center space-x-2 text-gray-700 py-2 whitespace-nowrap overflow-x-auto">
-                            <span className="font-medium text-gray-900">{index + 1}.</span>
-                            {tour.map((city, i) => (
-                                <span key={i} className="whitespace-nowrap">
-                                    {city} {i !== tour.length - 1 && <span className="text-gray-500">→</span>}
-                                </span>
-                            ))}
-                            <span className="ml-4 text-sm text-gray-600">
-                                (Fitness: {fitnessValues[index]?.toFixed(4)})
-                            </span>
-                        </div>
-                    ))}
-                </div>
+                <PopulationComponent
+                    population={population}
+                    fitnessValues={fitnessValues}
+                />
                 <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-3xl">
                     <h2 className="text-lg font-semibold text-gray-800">Selection - Roulette Wheel</h2>
                     <RouletteWheel
