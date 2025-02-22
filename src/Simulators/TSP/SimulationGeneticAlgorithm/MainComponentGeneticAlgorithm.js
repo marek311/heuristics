5f099235450a05b2da28fb304dc5568a0ffc26bf
marek311 <mareklacko311@icloud.com>
2025-02-22 10:24:06 +0100

    styling update

--- src/Simulators/TSP/SimulationGeneticAlgorithm/MainComponentGeneticAlgorithm.js
+++ src/Simulators/TSP/SimulationGeneticAlgorithm/MainComponentGeneticAlgorithm.js
@@ -82,7 +82,7 @@ function MainComponentGeneticAlgorithm() {
                     fitnessValues={fitnessValues}
                 />
                 <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-3xl">
-                    <h2 className="text-xl font-semibold text-center">Selection - Roulette Wheel</h2>
+                    <h2 className="text-xl font-semibold text-center">Selection</h2>
                     <RouletteWheel
                         fitnessValues={fitnessValues}
                         probabilities={probabilities}
