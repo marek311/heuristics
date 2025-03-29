864d6f47ba198196f2350850b8621e8439703faf
marek311 <mareklacko311@icloud.com>
2025-03-29 15:49:12 +0100

    exchange heuristic - first fit - run test

--- src/__tests__/AlgorithmsExchnageFirstFit.test.js
+++ src/__tests__/AlgorithmsExchnageFirstFit.test.js
@@ -81,4 +81,79 @@ describe('Algorithms - Exchange Heuristic - First Fit', () => {
         expect(initialState.setCurrentWeight).toHaveBeenCalledWith(10);
         expect(initialState.setCurrentPrice).toHaveBeenCalledWith(50);
     });
+
+    test('Perform Run', () => {
+        const items = [
+            { weight: 5, price: 10, originalIndex: 0 },
+            { weight: 5, price: 20, originalIndex: 1 },
+            { weight: 5, price: 10, originalIndex: 2 },
+            { weight: 5, price: 30, originalIndex: 3 },
+            { weight: 5, price: 50, originalIndex: 4 },
+        ];
+
+        const capacity = 10;
+
+        const state = {
+            currentBackpack: [items[0], items[1]],
+            currentNotBackpack: [items[2], items[3], items[4]],
+            currentWeight: 10,
+            currentPrice: 30,
+            setHighlightLinks: jest.fn(),
+            setOriginalIndexI: jest.fn(),
+            setOriginalIndexJ: jest.fn(),
+            setAdmissible: jest.fn(),
+            setImproving: jest.fn(),
+            setCurrentBackpack: jest.fn(),
+            setCurrentNotBackpack: jest.fn(),
+            setCurrentWeight: jest.fn(),
+            setCurrentPrice: jest.fn(),
+            setExchangeHistory: jest.fn(),
+            setIsCompleted: jest.fn(),
+            generateBinaryVector: jest.fn().mockReturnValue([1, 1, 0, 0, 0]),
+        };
+
+        run({
+            capacity,
+            currentBackpack: state.currentBackpack,
+            currentNotBackpack: state.currentNotBackpack,
+            currentWeight: state.currentWeight,
+            currentPrice: state.currentPrice,
+            setHighlightLinks: state.setHighlightLinks,
+            indexI: 0,
+            setOriginalIndexI: state.setOriginalIndexI,
+            indexJ: 0,
+            setOriginalIndexJ: state.setOriginalIndexJ,
+            setAdmissible: state.setAdmissible,
+            setImproving: state.setImproving,
+            setCurrentBackpack: state.setCurrentBackpack,
+            setCurrentNotBackpack: state.setCurrentNotBackpack,
+            setCurrentWeight: state.setCurrentWeight,
+            setCurrentPrice: state.setCurrentPrice,
+            setExchangeHistory: state.setExchangeHistory,
+            setIsCompleted: state.setIsCompleted,
+            generateBinaryVector: state.generateBinaryVector,
+            strategy: 'firstFit',
+            setBestFoundSolution: jest.fn(),
+            setBestFoundPrice: jest.fn(),
+            setBestFoundWeight: jest.fn(),
+        });
+
+        expect(state.setCurrentBackpack).toHaveBeenCalledWith(
+            expect.arrayContaining([
+                { weight: 5, price: 30, originalIndex: 3 },
+                { weight: 5, price: 50, originalIndex: 4 },
+            ])
+        );
+
+        expect(state.setCurrentNotBackpack).toHaveBeenCalledWith(
+            expect.arrayContaining([
+                { weight: 5, price: 10, originalIndex: 0 },
+                { weight: 5, price: 20, originalIndex: 1 },
+                { weight: 5, price: 10, originalIndex: 2 },
+            ])
+        );
+
+        expect(state.setCurrentWeight).toHaveBeenCalledWith(10);
+        expect(state.setCurrentPrice).toHaveBeenCalledWith(80);
+    });
 });
