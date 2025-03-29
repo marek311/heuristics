7574b060a85d0cace9cc51ba5c5730adaf8dd82b
marek311 <mareklacko311@icloud.com>
2025-03-29 19:31:55 +0100

    colors - centralization update

--- src/Main/Selector.js
+++ src/Main/Selector.js
@@ -4,6 +4,8 @@ import Colors from "./Colors";
 
 function Selector() {
 
+    const navigate = useNavigate();
+
     const modes = [
         {id: 'KnapsackInsert', name: 'Knapsack problem - Insert heuristic'},
         {id: 'KnapsackExchangeFirst', name: 'Knapsack problem - Exchange first fit'},
@@ -12,13 +14,9 @@ function Selector() {
         {id: 'TSPTabuSearch', name: 'TSP - Tabu search'},
         {id: 'TSPGenetic', name: 'TSP - Genetic algorithm'},
     ];
-
     const storedMode = localStorage.getItem('selectedMode');
     const initialMode = storedMode ? storedMode : modes[0].id;
-
     const [selectedMode, setSelectedMode] = useState(initialMode);
-
-    const navigate = useNavigate();
 
     const handleModeChange = (event) => {
         const newMode = event.target.value;
@@ -31,14 +29,14 @@ function Selector() {
     };
 
     return (
-        <div className="text-center p-6 bg-white rounded-lg shadow-lg flex flex-col items-center mt-4">
+        <div className={`text-center p-6 rounded-lg shadow-lg flex flex-col items-center mt-4 ${Colors.cardBackground}`}>
             <h1 className="text-4xl font-bold mb-6">Heuristics and Metaheuristics</h1>
-            <label className="mb-4 text-gray-800">
+            <label className={`mb-4 ${Colors.textPrimary}`}>
                 Mode:
                 <select
                     value={selectedMode}
                     onChange={handleModeChange}
-                    className="ml-2 p-2 border border-gray-300 rounded text-gray-800 focus:outline-none focus:ring-2">
+                    className={`ml-2 p-2 border rounded ${Colors.textPrimary} focus:outline-none focus:ring-2`}>
                     {modes.map((mode) => (
                         <option key={mode.id} value={mode.id}>
                             {mode.name}
