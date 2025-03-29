29df6758c06c5a2c1c63b43d5993bb92999bb00c
marek311 <mareklacko311@icloud.com>
2025-03-29 17:30:34 +0100

    exchange heuristic - best fit - run test

--- src/__tests__/AlgorithmsExchnageBestFit.test.js
+++ src/__tests__/AlgorithmsExchnageBestFit.test.js
@@ -1,4 +1,4 @@
-import { iteration } from '../Simulators/KP/ExchangeHeuristic/AlgorithmsExchange';
+import { iteration, run } from '../Simulators/KP/ExchangeHeuristic/AlgorithmsExchange';
 
 describe('Algorithms - Exchange Heuristic - Best Fit', () => {
 
@@ -78,5 +78,93 @@ describe('Algorithms - Exchange Heuristic - Best Fit', () => {
         });
         expect(initialState.setBestFoundPrice).toHaveBeenCalledWith(70);
         expect(initialState.setBestFoundWeight).toHaveBeenCalledWith(10);
+    });
+
+    test('Perform Run', () => {
+        const items = [
+            { weight: 5, price: 10, originalIndex: 0 },
+            { weight: 5, price: 20, originalIndex: 1 },
+            { weight: 5, price: 10, originalIndex: 2 },
+            { weight: 5, price: 30, originalIndex: 3 },
+            { weight: 5, price: 50, originalIndex: 4 },
+            { weight: 5, price: 70, originalIndex: 5 },
+        ];
+
+        const capacity = 10;
+
+        const state = {
+            currentBackpack: [items[0], items[1]],
+            currentNotBackpack: [items[2], items[3], items[4], items[5]],
+            currentWeight: 10,
+            currentPrice: 30,
+            bestFoundSolution: null,
+            bestFoundPrice: 0,
+            bestFoundWeight: 0,
+            setHighlightLinks: jest.fn(),
+            setOriginalIndexI: jest.fn(),
+            setOriginalIndexJ: jest.fn(),
+            setAdmissible: jest.fn(),
+            setImproving: jest.fn(),
+            setCurrentBackpack: jest.fn(),
+            setCurrentNotBackpack: jest.fn(),
+            setCurrentWeight: jest.fn(),
+            setCurrentPrice: jest.fn(),
+            setExchangeHistory: jest.fn(),
+            setIsCompleted: jest.fn(),
+            generateBinaryVector: jest.fn().mockReturnValue([1, 1, 0, 0, 0]),
+            setBestFoundSolution: jest.fn(),
+            setBestFoundPrice: jest.fn(),
+            setBestFoundWeight: jest.fn(),
+        };
+
+        run({
+            capacity,
+            currentBackpack: state.currentBackpack,
+            currentNotBackpack: state.currentNotBackpack,
+            currentWeight: state.currentWeight,
+            currentPrice: state.currentPrice,
+            setHighlightLinks: state.setHighlightLinks,
+            indexI: 0,
+            setOriginalIndexI: state.setOriginalIndexI,
+            indexJ: 0,
+            setOriginalIndexJ: state.setOriginalIndexJ,
+            setAdmissible: state.setAdmissible,
+            setImproving: state.setImproving,
+            setCurrentBackpack: state.setCurrentBackpack,
+            setCurrentNotBackpack: state.setCurrentNotBackpack,
+            setCurrentWeight: state.setCurrentWeight,
+            setCurrentPrice: state.setCurrentPrice,
+            setExchangeHistory: state.setExchangeHistory,
+            setIsCompleted: state.setIsCompleted,
+            generateBinaryVector: state.generateBinaryVector,
+            strategy: 'bestFit',
+            setBestFoundSolution: state.setBestFoundSolution,
+            setBestFoundPrice: state.setBestFoundPrice,
+            setBestFoundWeight: state.setBestFoundWeight,
+        });
+
+        expect(state.setCurrentBackpack).toHaveBeenCalledWith(
+            expect.arrayContaining([
+                { weight: 5, price: 50, originalIndex: 4 },
+                { weight: 5, price: 70, originalIndex: 5 },
+            ])
+        );
+
+        expect(state.setCurrentNotBackpack).toHaveBeenCalledWith(
+            expect.arrayContaining([
+                { weight: 5, price: 10, originalIndex: 0 },
+                { weight: 5, price: 20, originalIndex: 1 },
+                { weight: 5, price: 10, originalIndex: 2 },
+                { weight: 5, price: 30, originalIndex: 3 },
+            ])
+        );
+
+        expect(state.setCurrentWeight).toHaveBeenCalledWith(10);
+        expect(state.setCurrentPrice).toHaveBeenCalledWith(120);
+        expect(state.setIsCompleted).toHaveBeenCalledWith(true);
+
+        expect(state.setBestFoundSolution).toHaveBeenCalledWith(null);
+        expect(state.setBestFoundPrice).toHaveBeenCalledWith(0);
+        expect(state.setBestFoundWeight).toHaveBeenCalledWith(0);
     });
 });
